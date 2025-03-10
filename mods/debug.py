e4e0f9d412b0dcfd471bd1c7533dca07b52efe52
Mypzik3D <minermay.xyz@gmail.com>
2025-03-10 23:25:16 +0300

    move mods to site

--- mods/debug.py
+++ mods/debug.py
@@ -7,6 +7,24 @@ class Cog(commands.Cog, name="debug"):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @bot.command(name='ban')
+    @commands.has_permissions(ban_members=True)
+    async def ban(ctx, member: discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+        await ctx.send(f'{member.mention} banned: {reason}')
+
+    @commands.command(name='kick')
+    @commands.has_permissions(kick_members=True)
+    async def kick(ctx, member: discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+        await ctx.send(f'{member.mention} kicked: {reason}')
+
+    @commands.command(name='clear')
+    @commands.has_permissions(manage_messages=True)
+    async def clear(ctx, amount: int):
+        await ctx.channel.purge(limit=amount + 1)
+        await ctx.send(f'Удалено {amount} сообщений.', delete_after=5)
+
     @commands.command(name='sync')
     @commands.is_owner()
     async def slash_sync(self, ctx):
